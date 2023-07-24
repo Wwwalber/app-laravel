54366528d3f83e75c5011b63806402ee2ba2d63e
Walber Costa <walberoc@gmail.com>
2023-07-24 09:26:04 -0300

    traz dados p/ controller, com injeção

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -8,10 +8,7 @@ use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(){
-
-        //carregar dados
-        $support = new Support();
+    public function index(Support $support){ // com injeção de dependências
         // traz para um array
         $supports = $support->all(); // ($columns = ['*'])
         dd($supports); // se quiser debugar: com 'dd()', faz um 'var_dump' e chama 'die()'
